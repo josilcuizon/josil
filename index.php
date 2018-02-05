0398f5bacdd25c2dd7cc04203483223b449b0379
josilcuizon <36121096+josilcuizon@users.noreply.github.com>
2018-02-05 18:24:24 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+
+<title>WEB DESIGN 1</title>
+</head>
+<body style="background-color:green;">
+
+<center><h1 style="background-color:red;">PERSONAL DATA</h1></center>
+<p><h2>NAME:<u>Lejosil P. Cuizon</u></h2></p>
+<p><h2>SEX:<u> Male</u></h2></p>
+<p><h2>ADDRESS:<u>Tangke City of Naga  </u></h2></p>
+<p><h2>DATE OF BIRTH:<u>December 9,1988</u></h2><p>
+<p><h2>CIVIL STATUS:<u>Single</u></h2></p>
+<p><h2>RELIGION:<u>Roman Catholic</u></h2></p>
+<p><h2>CITIZENSHIP:<u>Filipino</u></h2></p>
+<p><h2>EMAIL ADDRESS:<u>josil.cuizon@yahoo.com</u></h2></p><hr>
+<center><h1 style="background-color:red">EDUCATIONAL ATTAINMENT</h1></center>
+<p><h2>ELEMENTARY:<u>Naga Central Elementary School</u></h2></p>
+<p><h2>HIGH SCHOOL:<u>Naga National High School</u></h2></p>
+<p><h2>COLLEGE:<u>Cebu Technological University</u></h2></p><hr>
+<center><h1 style="background-color:red">BIOGRAPHY</h1></center>
+<p><h2>I am Lejosil P. Cuizon, 29 yrs of age and now living in Tangke City of Naga,Cebu.
+ I was Graduated high school in Naga National High School.
+ I am the oldest amoung three siblings , after i graduarted high school,
+ I worked for about ten years in different Companies and then i
+ decided to go back in studying beacause i want to purse my dreams and help my family.
+ 
+</h2></p>
+</body>
+</html>
+
+
+
